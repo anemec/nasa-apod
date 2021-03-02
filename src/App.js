a6b637ddffd7971b9759a9a6e7dde06b205fc85b
Andrew Nemec <andrewnemec2@gmail.com>
2021-03-02 16:45:07 -0600

    AN - Update explanation styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Picture from "./components/Picture";
 function App() {
   return (
     <div className="App">
-      <header className="header">This is a header</header>
+      <header className="header">NASA Astronomy Picture Of The Day</header>
       <div className="container">
         <Picture />
       </div>
--- src/components/ImageLoad.js
+++ src/components/ImageLoad.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ImageLoad = (props) => {
   function loadImg() {}
+  console.log(props.isFlipped);
   return (
     <>
-      {props.isLoading ? (
-        <p>Loading...</p>
+      {props.isFlipped ? (
+        <div className="picture-card__explanation">
+          <h1>Explanation</h1>
+          <p>{props.data.explanation}</p>
+        </div>
       ) : (
         <img
-          className='picture-card__image'
+          className="picture-card__image"
           src={props.data.hdurl}
           alt={props.data.title}
         />
